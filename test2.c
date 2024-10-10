6a56d57688eb8bc7cf0fe4934c3f81a85e6c4d54
jjinsoo93 <140279733+jjinsoo93@users.noreply.github.com>
2024-10-10 17:15:08 +0000

    timer

--- test2.c
+++ test2.c
@@ -7,9 +7,16 @@ int main(int argc, char *argv[]) {
     int i, rank, size;
     double x, pi, sum = 0.0, local_sum = 0.0;
     double step = 1.0/(double) NUMSTEPS;
+    double start_time, end_time, elapsed_time;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Start the timer (only need one timer for the whole program)
+    if (rank == 0) {
+        start_time = MPI_Wtime();
+    }
 
     // Divide the work among processes
     for (i = rank; i <= NUMSTEPS; i += size) {
@@ -20,10 +27,14 @@ int main(int argc, char *argv[]) {
     // Sum all partial results to calculate pi
     MPI_Reduce(&local_sum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    // Finalize pi calculation and time reporting only in the master process
+    // Stop the timer and calculate elapsed time in nanoseconds
     if (rank == 0) {
+        end_time = MPI_Wtime();
+        elapsed_time = (end_time - start_time) * 1e9;  // Convert to nanoseconds
+
         pi = step * sum;
         printf("PI is %.20f\n", pi);
+        printf("Total elapsed time = %.0f nanoseconds\n", elapsed_time);
     }
 
     MPI_Finalize();
